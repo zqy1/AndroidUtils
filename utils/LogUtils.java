ff648323522573770f67481988fdb824e55f3d9f
Mr Z <805892567@qq.com>
2017-11-11 10:30:48 -0600

    Add files via upload

---
+++ utils/LogUtils.java
@@ -0,0 +1,48 @@
+package com.example.zqy.myapplication.utils;
+
+import android.util.Log;
+
+/**
+ * Created by zqy on 17-11-3.
+ */
+
+public class LogUtils {
+
+    public static final int VERBOSE = 1;
+    public static final int DEBUG = 2;
+    public static final int INFO = 3;
+    public static final int WARN = 4;
+    public static final int ERROR = 5;
+    public static final int NOTHING = 6;
+    public static int level = VERBOSE;
+
+//    public LogUtils() {}
+
+    public static void v(String tag, String msg) {
+        if (level <= VERBOSE) {
+            Log.v(tag, msg);
+        }
+    }
+    public static void d(String tag, String msg) {
+        if (level <= DEBUG) {
+            Log.d(tag, msg);
+        }
+    }
+    public static void i(String tag, String msg) {
+        if (level <= INFO) {
+            Log.i(tag, msg);
+        }
+    }
+    public static void w(String tag, String msg) {
+        if (level <= WARN) {
+            Log.w(tag, msg);
+        }
+    }
+    public static void e(String tag, String msg) {
+        if (level <= ERROR) {
+            Log.w(tag, msg);
+        }
+    }
+
+
+}
---
+++ utils/ToastUtils.java
@@ -0,0 +1,30 @@
+package com.example.zqy.myapplication.utils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by zqy on 17-10-27.
+ */
+
+public class ToastUtils {
+
+    private ToastUtils() {}
+
+    public static void show(CharSequence text, int duration, Context context) {
+        Toast.makeText(context, text, duration).show();
+    }
+
+    public static void show(int resId, int duration, Context context) {
+        show(context.getText(resId), duration, context);
+    }
+
+    public static void show(CharSequence text, Context context) {
+        show(text, Toast.LENGTH_SHORT, context);
+    }
+
+    public static void show(int resId, Context context) {
+        show(context.getText(resId), context);
+    }
+
+}
